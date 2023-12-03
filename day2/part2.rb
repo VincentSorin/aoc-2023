178e80aa99cf954be17e8b8131843a6011bb6c72
Vincent Sorin <vincent.sorin@doctolib.com>
2023-12-03 21:39:19 +0100

    day 2 part 2

--- day2/part2.rb
+++ day2/part2.rb
@@ -3,10 +3,6 @@
 file_path = File.expand_path("../input.txt", __FILE__)
 input     = File.read(file_path)
 
-MAXIMUM_RED = 12
-MAXIMUM_GREEN = 13
-MAXIMUM_BLUE = 14
-
 input
   .each_line
   .sum { |line|
@@ -14,11 +10,7 @@ input
     green = line.scan(/(\d+) green/).flatten.map(&:to_i).max
     blue  = line.scan(/(\d+) blue/).flatten.map(&:to_i).max
 
-    if blue > MAXIMUM_BLUE || red > MAXIMUM_RED || green > MAXIMUM_GREEN
-      0
-    else
-      line.scan(/Game (\d*)/).flatten.first.to_i
-    end
+    red * green * blue
   }
   .tap { puts _1 }
 
