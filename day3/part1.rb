5b5e832d1142e91eeec458d1c727cc14f0b10f24
Vincent Sorin <vincent.sorin@doctolib.com>
2023-12-04 00:28:53 +0100

    day 3 improvement

--- day3/part1.rb
+++ day3/part1.rb
@@ -7,28 +7,25 @@ def part1(input)
   numbers = []
   input
     .split("\n")
-    .each_with_index do |line, index|
+    .each_with_index do |line, line_number|
       line.scan(/\d+/) do |number|
-        offset = $~.offset(0)
-        if had_symbol_around(input.split("\n"), index, offset)
-          numbers << number
-        end
+        numbers << number if had_symbol_around(input.split("\n"), line_number, $~.offset(0))
       end
     end
   numbers.map(&:to_i).sum
 end
 
-def had_symbol_around(input, index, offset)
+def had_symbol_around(input, line_number, offset)
   # offset for width
   min_offset = [offset[0] - 1, 0].max
-  max_offset = [offset[1] + 1, input[index].length].min - 1
+  max_offset = [offset[1] + 1, input[line_number].length].min - 1
   # offset for height
-  min_index = [index - 1, 0].max
-  max_index = [index + 1, input.length - 1].min
+  min_line = [line_number - 1, 0].max
+  max_line = [line_number + 1, input.length - 1].min
   
-  (min_index..max_index).each do |current_index|
+  (min_line..max_line).each do |current_index|
     input[current_index][min_offset..max_offset].each_char do |char|
-      return true if char&.match?(/[^#{%w[. 1 2 3 4 5 6 7 8 9 0].join}]/)
+      return true if char&.match?(/[^.\d]/)
     end
   end
   
