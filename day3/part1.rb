ae20c31091e72c637ce322341485a66bb7f24565
Vincent Sorin <vincent.sorin@doctolib.com>
2023-12-04 00:07:22 +0100

    fix style

--- day3/part1.rb
+++ day3/part1.rb
@@ -26,11 +26,11 @@ def had_symbol_around(input, index, offset)
   min_index = [index - 1, 0].max
   max_index = [index + 1, input.length - 1].min
   
-  (min_index..max_index).each { |current_index|
+  (min_index..max_index).each do |current_index|
     input[current_index][min_offset..max_offset].each_char do |char|
       return true if char&.match?(/[^#{%w[. 1 2 3 4 5 6 7 8 9 0].join}]/)
     end
-  }
+  end
   
   false 
 end
