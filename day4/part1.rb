e036b4c566bd905128edf75e91694f6b723bbbba
Vincent Sorin <vincent.sorin@doctolib.com>
2023-12-04 10:10:22 +0100

    day 4 part 1

---
+++ day4/part1.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../input.txt", __FILE__)
+input     = File.read(file_path)
+
+input
+  .split("\n")
+  .sum do |card|
+    winning_cards = card.split(':')[1].split('|')[0].split(' ').map(&:to_i)
+    owned_cards = card.split(':')[1].split('|')[1].split(' ').map(&:to_i)
+    owned_winning_cards = winning_cards & owned_cards
+    (2 ** ((winning_cards & owned_cards).length - 1)).floor
+  end.tap { puts _1 }
